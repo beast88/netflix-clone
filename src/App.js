76f5eaef397e666340496487df24956d0fe55930
beast88 <progers999@hotmail.co.uk>
2021-10-12 14:14:49 +0100

    Jumbotron has been built

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import jumboData from './fixtures/jumbotron.json';
+import Jumbotron from './components/jumbotron';
 
 const App = () => {
   return (
-    <div>
-      <h1>Netflix</h1>
-    </div>
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.Subtitle>{item.subtitle}</Jumbotron.Subtitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 };
 
